529edcaa6ab8448e2cc3c362e63b4c2df9157dc0
Santiago Urbaneja <surbaneja24@gmail.com>
2026-04-25 19:16:43 +0000

    Proyecto mejorado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,36 +1,56 @@
 import { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 const Home = () => {
-	const [list, setList] = useState(["horacio", "luis", "daniel", "maria", "jose"]);
-	const [input, setInput] = useState("");
+    const [list, setList] = useState(["horacio", "luis", "daniel", "maria", "jose"]);
+    const [input, setInput] = useState("");
 
-	function handleClick() {
-		if (input.trim() !== "") {
-			setList([...list, input]);
-			setInput("");
-		}
-	}
+    function handleClick() {
+        if (input.trim() !== "") {
+            setList([...list, input]);
+            setInput("");
+        }
+    }
 
-	return (
-		<div className="text-center">
-			<h1>Lista de usuarios</h1>
+    function borrarUsuario(indice) {
+        const nuevaLista = list.filter((user, i) => i !== indice);
+        setList(nuevaLista);
+    }
 
-			<input
-				value={input}
-				onChange={(e) => setInput(e.target.value)}
-				placeholder="Nombre..."
-			/>
-			<button onClick={handleClick}>Agregar</button>
+    return (
+        <div className="contenedor-principal">
+            <div className="text-center">
+                <h1>Lista de usuarios</h1>
 
-			{
-				list.map((user, i) => {
-					return (
-						<p key={i}>{user} {i}</p>
-					)
-				})
-			}
-		</div>
-	);
+                <input
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                    placeholder="Nombre..."
+                />
+                
+                <button className="btn-agregar" onClick={handleClick}>
+                    <FontAwesomeIcon icon={faPlus}/>
+                </button>
+
+                {
+                    list.map((user, i) => {
+                        return (
+                            <div className="caja-usuario" key={i}>
+                                <p>{user}</p>
+                                
+                                <button className="btn-eliminar" onClick={() => borrarUsuario(i)}>
+                                    <FontAwesomeIcon icon={faXmark} />
+                                </button>
+                            </div>
+                        )
+                    })
+                }
+                
+                <div className="contador">{list.length} usuarios en la lista</div>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
